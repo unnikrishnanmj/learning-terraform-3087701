ecee1cf20451e65435c34b3e12ca0550f18c9dd1
unnikrishnanmj <unnikrishnan.mj@gmail.com>
2025-05-06 19:39:29 +0530

    Secrity group Changes

--- main.tf
+++ main.tf
@@ -14,11 +14,32 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default"  {
+  default  =  true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  vpc_security_groups_ids = [aws_security_group.id]
   tags = {
     Name = "LI-TF-Course"
   }
+}
+
+resource "aws_security_group" "web"  {
+  name  = "web"
+  desc  = "allowed http inbound and outbound everthing"
+  
+  vpc.id  = data.aws_vpc.default.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in"{
+  type  =  "ingress"
+  from_port  =  8080
+  to_port  =  80
+  
+  cidr_blocks  = [0.0.0.0/0]
+  protocol  =  "tcp"
+  aws_security_group.id  =  aws_security_group.web.id
 }
